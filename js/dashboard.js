580854b5b2deb174c04c49eb6fa8b03965b2c18f
Reyhan Tri R <reyhanramadan21@smk.belajar.id>
2023-11-20 20:42:46 +0700

    penambahan fitur active link di sidebar

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,3 +11,20 @@ collapse.addEventListener("hidden.bs.collapse", function () {
   // document.body.style.paddingRight = "0";
   stickyTop.style.overflow = "hidden";
 });
+
+// Active
+$(document).ready(function () {
+  // Dapatkan URL halaman saat ini
+  var currentLocation = window.location.href;
+
+  // Loop melalui setiap tautan di dalam sidebar
+  $("#sidebarMenu a").each(function () {
+    var linkHref = $(this).attr("href");
+
+    // Bandingkan URL halaman saat ini dengan href tautan di sidebar
+    if (currentLocation.includes(linkHref)) {
+      // Jika cocok, tambahkan kelas 'active'
+      $(this).addClass("active");
+    }
+  });
+});
